d0995bb9bfe181b42c402d713cc0b56c9d1387bc
guofucheng <1024433742@qq.com>
2022-07-01 15:54:50 +0800

    demo初始化

---
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -0,0 +1,16 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : demo
+ * @description :
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
